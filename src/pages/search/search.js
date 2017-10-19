31b8cc37d3ffb3853a19cabfbb288a4aa8ddf1a6
twlk28 <twlk28@gamil.com>
2017-10-19 21:49:48 +0800

    迁移静态文件: search(商品列表), member(个人中心), cart

---
+++ src/pages/search/search.js
@@ -0,0 +1,64 @@
+import 'css/common.css'
+import './search.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+// import log from 'js/utils.js'
+
+// ui
+// import { InfiniteScroll } from 'mint-ui'
+// Vue.use(InfiniteScroll)
+
+// component
+import Foot from 'components/Foot.vue'
+// import Swipe from 'components/Swipe.vue'
+
+// utils
+const log = console.log.bind(console)
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         lists: null,
+//         banners: null,
+//         pageNum: 1,
+//         pageSize: 6,
+//         loading: false,
+//         allLoaded: false,
+//     },
+//     created() {
+//         this.requestHotSales()
+//         this.requestBanners()
+//     },
+//     methods: {
+//         requestHotSales(){
+//             if (this.allLoaded){
+//                 return
+//             }
+//             this.loading = true
+//             axios.post(url.hotSales, {
+//                 pageNum: this.pageNum,
+//                 pageSize: this.pageSize,
+//             }).then(res=>{
+//                 let hotSales = res.data.lists
+//                 if (hotSales.length < this.pageSize){
+//                     this.allLoaded = true
+//                 }
+//                 this.lists = this.lists? this.lists.concat(hotSales) : hotSales
+//                 this.loading = false
+//                 this.pageNum += 1
+//             })
+//         },
+//         requestBanners(){
+//             axios.get(url.banners)
+//             .then(res => {
+//                 this.banners = res.data.lists
+//             })
+//         }
+//     },
+//     components: {
+//         Foot,
+//         Swipe,
+//     }
+// })
