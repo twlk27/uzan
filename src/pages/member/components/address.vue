4cd2b2d61892bf796aba2270ec6d7f08c61b6b6a
twlk28 <twlk28@gamil.com>
2017-10-22 09:06:24 +0800

    添加路由

---
+++ src/pages/member/components/address.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <div class="">
+      地址管理
+  </div>
+</template>
+
+<style lang="css">
+@import "./address_base.css";
+@import "./address.css";
+
+</style>
--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,62 +1,29 @@
-import './member_base.css'
-import './member.css'
-
 import Vue from 'vue'
+import Router from 'vue-router'
 import axios from 'axios'
 import url from 'js/api.js'
 import utils, {log} from 'js/utils.js'
+Vue.use(Router)
 
-// ui
-// import { InfiniteScroll } from 'mint-ui'
-// Vue.use(InfiniteScroll)
+// route.vue
+import MemberX from './components/member.vue'
+import Address from './components/address.vue'
 
-// component
-import Foot from 'components/Foot.vue'
+let routes = [{
+    path: '/',
+    component: MemberX
+},{
+    path: '/address',
+    component: Address
+}]
 
+// 创建router实例
+let router = new Router({
+    routes
+})
 
-
-// new Vue({
-//     el: '#app',
-//     data: {
-//         lists: null,
-//         banners: null,
-//         pageNum: 1,
-//         pageSize: 6,
-//         loading: false,
-//         allLoaded: false,
-//     },
-//     created() {
-//         this.requestHotSales()
-//         this.requestBanners()
-//     },
-//     methods: {
-//         requestHotSales(){
-//             if (this.allLoaded){
-//                 return
-//             }
-//             this.loading = true
-//             axios.post(url.hotSales, {
-//                 pageNum: this.pageNum,
-//                 pageSize: this.pageSize,
-//             }).then(res=>{
-//                 let hotSales = res.data.lists
-//                 if (hotSales.length < this.pageSize){
-//                     this.allLoaded = true
-//                 }
-//                 this.lists = this.lists? this.lists.concat(hotSales) : hotSales
-//                 this.loading = false
-//                 this.pageNum += 1
-//             })
-//         },
-//         requestBanners(){
-//             axios.get(url.banners)
-//             .then(res => {
-//                 this.banners = res.data.lists
-//             })
-//         }
-//     },
-//     components: {
-//         Foot,
-//         Swipe,
-//     }
-// })
+// 跟组件注入
+new Vue({
+    el: "#app",
+    router
+})
