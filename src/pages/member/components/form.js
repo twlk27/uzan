d04604836ddfc429a052b5968305c98bec7e3b96
twlk28 <twlk28@gamil.com>
2017-10-22 12:09:19 +0800

    地址三级联动

--- src/pages/member/components/form.js
+++ src/pages/member/components/form.js
@@ -1,3 +1,5 @@
+import addressData from 'js/address.json'
+
 export default {
     data() {
         return {
@@ -10,17 +12,40 @@ export default {
             id: '',
             type: '',
             instance: '',
+            provinces: addressData.list,
+            citys: null,
+            districts: null,
         }
     },
     created(){
         let q = this.$route.query
         this.type = q.type
         this.instance = q.instance
+
         let e = q.instance
         if (q.type == 'edit'){
             this.name = e.name
             this.tel = e.tel
             this.address = e.address
+        }
+    },
+    watch: {
+        provinceValue(val, oldVal){
+            if (val == -1) return
+            let i = this.provinces.findIndex(item => {
+                return item.value == val
+            })
+            this.citys = this.provinces[i].children
+            this.cityValue = -1
+            this.districtValue = -1
+        },
+        cityValue(val, oldVal){
+            if (val == -1) return
+            let i = this.citys.findIndex(item => {
+                return item.value == val
+            })
+            this.districts = this.citys[i].children
+            this.districtValue = -1
         }
     }
 }
